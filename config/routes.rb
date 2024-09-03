8b84d9e1377549c279afda49ae59a48109ac6963
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-09-03 15:06:57 -0300

    constraint json

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles, only: :index
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #resources :articles, only: :index, constraints: lambda { |req| ['json', 'xml'].include? req.format }
+  # :constraints para especificar um formato obrigatório, no caso em json e xml, incluídos no GET 127.0.0.1:3000/articles.json OU .xml
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :articles, only: :index, constraints: lambda { |req| %w[json].include? req.format }
+  # %w = array em string
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
