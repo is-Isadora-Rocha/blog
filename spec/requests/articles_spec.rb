d614b6f0ab29dbbe47ef6fd220d52a81d8ee4eec
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-09-03 16:29:38 -0300

    teste spec

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles' do
-  context "GET" do
-    it '/articles' do
-      get '/articles.json'
+  before(:each) { Article.create(title: 'art 2', content: 'conteudo do art 2') }
 
-      #expect(response).to have_http_status(:ok)
-      expect(response).to have_http_status(:accepted) #status 202
+  context "GET /articles" do
+    it 'responds with status Ok' do
+      get '/articles.json'
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'responds with correct persistes article json' do
+      get '/articles.json'
+      expect(response.body).to include('art 2')
+      expect(response.body).to include('conteudo do art 2')
     end
   end
 end
