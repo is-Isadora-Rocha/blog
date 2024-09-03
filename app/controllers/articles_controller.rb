cf67b93288513dbd9f829e1f507c21ae09b4ca0b
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-09-03 14:50:37 -0300

    reponse to html and json

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   def index
     Article.all
+    respond_to do |format|
+      format.html {}
+      format.json{ render json: { message: 'carregado'} }
+    end
   end
 end
